53fc02fd0a1bbb2586f33fef962f9e40148b9b28
fesine <240944773@qq.com>
2019-12-13 14:31:15 +0800

    并发v4提交，使用wait和notify实现资源交替生产和消费，满足需求。

--- src/main/java/com/fesine/concurrent/ShareResource.java
+++ src/main/java/com/fesine/concurrent/ShareResource.java
@@ -13,19 +13,32 @@ public class ShareResource {
     private String name;
     private String gender;
 
+    //增加标志位，判断资源池是否为空，true为空
+    private boolean isEmpty = true;
+
     /**
      * 模拟生产者向共享资源对象中存储数据
      * @param name
      * @param gender
      */
     synchronized public void push(String name,String gender){
-        this.name=name;
         try {
+            while (!isEmpty){
+                //当共享资源池不为空的时候，则等待消费者来消费
+                //使用同步锁对象来调用，表示当前线程释放同步锁，只能被其他线程唤醒
+                this.wait();
+            }
+            //开始生产
+            this.name=name;
             Thread.sleep(10L);
+            this.gender=gender;
+            //生产结束
+            isEmpty = false;
+            //唤醒一个消费者来消费
+            this.notify();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        this.gender=gender;
     }
 
     /**
@@ -33,7 +46,15 @@ public class ShareResource {
      */
     synchronized public void popup(){
         try {
+            while (isEmpty) {
+                //为空则等待生产者来生产
+                //使用同步锁对象来调用，表示当前线程释放同步锁，进入等待池，只能被其他线程唤醒
+                this.wait();
+            }
             Thread.sleep(10L);
+            isEmpty = true;
+            //消费结束，唤醒一个生产者来生产
+            this.notify();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
