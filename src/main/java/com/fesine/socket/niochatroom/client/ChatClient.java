ec6ce672481a0ddd914117c2d45325dea81fc5fd
fesine <240944773@qq.com>
2020-06-04 00:04:13 +0800

    基于nio实现多人聊天室功能。

---
+++ src/main/java/com/fesine/socket/niochatroom/client/ChatClient.java
@@ -0,0 +1,146 @@
+package com.fesine.socket.niochatroom.client;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Set;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2020/5/27
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2020/5/27
+ */
+public class ChatClient {
+
+    private static final Integer DEFAULT_SERVER_PORT = 8081;
+
+    private static final String DEFAULT_SERVER_HOST = "127.0.0.1";
+
+    private static final String QUIT = "quit";
+
+    private int BUFFER = 1024;
+
+    private SocketChannel client;
+
+    private Selector selector;
+
+
+    private ByteBuffer rBuffer = ByteBuffer.allocate(BUFFER);
+
+    private ByteBuffer wBuffer = ByteBuffer.allocate(BUFFER);
+
+    private Charset charset = Charset.forName("UTF-8");
+
+    private String host;
+
+    private int port;
+
+    public ChatClient() {
+        this(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT);
+    }
+
+    public ChatClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    /**
+     * TODO 向服务器发送消息
+     * @param msg
+     * @throws IOException
+     */
+    public void send(String msg) throws IOException {
+    }
+
+    /**
+     * TODO 从服务器接收消息
+     * @return
+     * @throws IOException
+     */
+    private String receive(SocketChannel client) throws IOException {
+        String msg = null;
+        return msg;
+    }
+
+    /**
+     * 检查客户端是否准备退出
+     * @param msg
+     * @return
+     */
+    public boolean readyToQuit(String msg) {
+        return QUIT.equals(msg);
+    }
+
+    /**
+     * 关闭服务端socket
+     */
+    public void close(Closeable closeable) {
+        try {
+            closeable.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void start(){
+        try {
+            client = SocketChannel.open();
+            client.configureBlocking(false);
+            client.connect(new InetSocketAddress(host, port));
+            client.register(selector, SelectionKey.OP_CONNECT);
+            System.out.println("客户端启动，端口：" + port + "...");
+            while (true) {
+                //阻塞等待事件
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                for (SelectionKey selectedKey : selectedKeys) {
+                    handles(selectedKey);
+                }
+                //清空事件
+                selectedKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(selector);
+        }
+    }
+
+    private void handles(SelectionKey selectedKey) throws IOException {
+        //connect事件-连接就绪
+        if (selectedKey.isConnectable()) {
+            SocketChannel client = (SocketChannel) selectedKey.channel();
+            if (client.isConnectionPending()) {
+                client.finishConnect();
+                //处理用户的输入
+                new Thread(new UserInputHandler(this)).start();
+            }
+            client.register(selector, SelectionKey.OP_READ);
+        } else if (selectedKey.isReadable()) {
+            //read事件-服务器转发消息
+            SocketChannel client = (SocketChannel) selectedKey.channel();
+            String msg = receive(client);
+            if (msg.isEmpty()) {
+                //服务器异常
+                close(selector);
+            }else{
+                System.out.println(msg);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient();
+        client.start();
+    }
+}
---
+++ src/main/java/com/fesine/socket/niochatroom/server/ChatServer.java
@@ -0,0 +1,176 @@
+package com.fesine.socket.niochatroom.server;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Set;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2020/5/27
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2020/5/27
+ */
+public class ChatServer {
+
+    private static final int DEFAULT_PORT = 8081;
+
+    private static final String QUIT = "quit";
+
+    private int BUFFER = 1024;
+
+    private ServerSocketChannel server;
+
+    private Selector selector;
+
+
+    private ByteBuffer rBuffer = ByteBuffer.allocate(BUFFER);
+
+    private ByteBuffer wBuffer = ByteBuffer.allocate(BUFFER);
+
+    private Charset charset = Charset.forName("UTF-8");
+
+    private int port;
+
+    public ChatServer(){
+        this(DEFAULT_PORT);
+    }
+
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        try {
+            server = ServerSocketChannel.open();
+            server.configureBlocking(false);
+            server.bind(new InetSocketAddress(port));
+
+            selector = Selector.open();
+            //监听accept事件
+            server.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("服务器启动，监听端口："+port+"...");
+            while (true) {
+                //阻塞等待事件
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                for (SelectionKey selectedKey : selectedKeys) {
+                    handles(selectedKey);
+                }
+                //清空事件
+                selectedKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            close(selector);
+        }
+    }
+
+    private void handles(SelectionKey selectedKey) throws IOException {
+        //accept事件-和客户端建立了连接
+        if (selectedKey.isAcceptable()) {
+            ServerSocketChannel server = (ServerSocketChannel) selectedKey.channel();
+            SocketChannel client = server.accept();
+            client.configureBlocking(false);
+            //注册read事件
+            client.register(selector, SelectionKey.OP_READ);
+            System.out.println(getClientName(client)+"已连接.");
+        } else if (selectedKey.isReadable()) {
+            //处理read事件
+            SocketChannel client = (SocketChannel) selectedKey.channel();
+            String fwdMsg = receive(client);
+            if (fwdMsg.isEmpty()) {
+                //取消监听
+                selectedKey.cancel();
+                //selector唤醒
+                selector.wakeup();
+            }else{
+                forwardMessage(client, fwdMsg);
+                //判断是否是退出
+                if (readyToQuit(fwdMsg)) {
+                    //取消监听
+                    selectedKey.cancel();
+                    //selector唤醒
+                    selector.wakeup();
+                    System.out.println(getClientName(client) + "已断开.");
+                }
+            }
+        }
+    }
+
+    /**
+     * 转发消息
+     * @param client
+     * @param fwdMsg
+     */
+    private void forwardMessage(SocketChannel client, String fwdMsg) throws IOException {
+        //通过selector keys获取所有客户端
+        for (SelectionKey key : selector.keys()) {
+            Channel connectedClient = key.channel();
+            if (connectedClient instanceof ServerSocketChannel) {
+                continue;
+            }
+            if (key.isValid()) {
+                wBuffer.clear();
+                String pre;
+                if (client.equals(connectedClient)) {
+                    pre = "[me]";
+                }else {
+                    pre = getClientName(client);
+                }
+                wBuffer.put(charset.encode(pre+":" + fwdMsg));
+                //由写转为读
+                wBuffer.flip();
+                while (wBuffer.hasRemaining()) {
+                    ((SocketChannel)connectedClient).write(wBuffer);
+                }
+            }
+        }
+    }
+
+    /**
+     * 读取数据
+     * @param client
+     * @return
+     */
+    private String receive(SocketChannel client) throws IOException {
+        rBuffer.clear();
+        while (client.read(rBuffer) > 0) {
+        }
+        return String.valueOf(charset.decode(rBuffer));
+    }
+
+    private String getClientName(SocketChannel client) {
+        return "客户端[" + client.socket().getPort() + "]";
+    }
+
+
+    /**
+     * 关闭服务端socket
+     */
+    public synchronized void close(Closeable closeable){
+        try {
+            closeable.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean readyToQuit(String msg) {
+        return QUIT.equals(msg);
+    }
+
+
+
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer();
+        server.start();
+    }
+}
